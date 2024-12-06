8059c3177f9dc7bf7ee464dcf95fcd76ffc90b74
nissha29 <nishakashyap2907@gmail.com>
2024-12-06 21:15:18 +0530

    type change in config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
-module.exports = {
+// tailwind.config.js
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+      "./src/**/*.{html,js}",
+      "./public/index.html",
   ],
   theme: {
-    extend: {},
+      extend: {},
   },
   plugins: [],
-}
+};
